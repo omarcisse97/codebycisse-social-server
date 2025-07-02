495b385e8b23375a9c5836203c0a4e9583468306
omar cisse <omarcisse97@knights.ucf.edu>
2025-07-02 05:09:40 -0400

    Added query param as acceptable headers

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'api_key'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'api_key', 'q'],
 }));
 
 app.use(express.json());
